84043d0ca857dda050172e6f015c7da43954cd38
mmoo <knr524@gmail.com>
2023-02-02 10:57:21 +0900

    Time: 658 ms (28.50%), Space: 43 MB (61.67%) - LeetHub

--- 0125-valid-palindrome/0125-valid-palindrome.java
+++ 0125-valid-palindrome/0125-valid-palindrome.java
@@ -1,7 +1,10 @@
 class Solution {
     public boolean isPalindrome(String s) {
         String w = s.toLowerCase().replaceAll("[^A-Za-z0-9]","");
-        String rev = new StringBuffer(w).reverse().toString();
-        return w.equals(rev);
+        if(w.length()==1) return true;
+        for(int i=0;i<w.length()/2;i++){
+           if(w.charAt(i) != w.charAt(w.length()-1-i)) return false;
+        }
+        return true;
     }
 }
