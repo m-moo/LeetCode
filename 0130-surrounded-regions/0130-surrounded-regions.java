3566920cddc721f322fb76c0925d017f0799043b
mmoo <knr524@gmail.com>
2023-04-19 01:17:26 +0900

    Time: 0 ms (100.00%), Space: 44.5 MB (77.67%) - LeetHub

--- 0130-surrounded-regions/0130-surrounded-regions.java
+++ 0130-surrounded-regions/0130-surrounded-regions.java
@@ -1,46 +1,37 @@
 class Solution {
     public void solve(char[][] board) {
         int[][] dir = new int[][]{{0, 1}, {0, -1}, {1, 0}, {-1, 0}};
-        int answer = -1;
-        
         int r = board.length;
         int c = board[0].length;
         Queue<int[]> startP = new LinkedList<>();
-        /* 테두리 O 체크 */
+        /* 테두리 O를 dfs로 P로 변경 */
         for(int i=0;i<c;i++) {
-            if(board[0][i] == 'O') startP.add(new int[]{0,i});
-            if(board[r-1][i] == 'O') startP.add(new int[]{r-1,i});
+            if(board[0][i] == 'O') dfs(board,0,i);
+            if(board[r-1][i] == 'O') dfs(board,r-1,i);
         }
         for(int i=0;i<r;i++) {
-            if(board[i][0] == 'O') startP.add(new int[]{i,0});
-            if(board[i][c-1] == 'O') startP.add(new int[]{i,c-1});
+            if(board[i][0] == 'O') dfs(board,i,0);
+            if(board[i][c-1] == 'O') dfs(board,i,c-1);
         }
 
-        /* 테두리 O를 1로 변경 */
-        while(!startP.isEmpty()) {
-            int[] curr = startP.poll();
-            board[curr[0]][curr[1]] = '1';
+        swap(board,'O','X');
+        swap(board,'P','O');
+    }
+    
+    /* 글자 변환 */
+    public void swap(char[][] board, char origin, char to) {
+        for(int i=0;i< board.length;i++)
+            for(int j=0;j<board[0].length;j++)
+                if(board[i][j] == origin) board[i][j] = to;
+    }
 
-            /* 'O' 타일 사방 탐색, 다음 진행 포인트 적재 */
-            for(int i=0;i< dir.length;i++) {
-                int nx = curr[0]+dir[i][0];
-                int ny = curr[1]+dir[i][1];
-                
-                if(nx > -1 && ny > -1 && nx < board.length && ny < board[0].length
-                    && board[nx][ny] == 'O') {
-                    board[nx][ny] = '1';
-                    startP.add(new int[]{nx,ny});
-                }
-            }
-        }
-        
-        /* 1 타일은 O로, O 타일은 X로 변환 */
-        for(int i=0;i<board.length;i++) {
-            
-            for(int j=0;j<board[0].length;j++) {
-                if(board[i][j] == '1') board[i][j] = 'O';
-                else if(board[i][j] == 'O') board[i][j] = 'X';
-            }
-        }
+    /* O를 P로 변환 */
+    public void dfs(char[][] board, int i, int j) {
+        if(i < 0 || j < 0 || i >= board.length || j >= board[0].length || board[i][j] != 'O') return;
+        board[i][j] = 'P';
+        dfs(board,i,j+1);
+        dfs(board,i+1,j);
+        dfs(board,i-1,j);
+        dfs(board,i,j-1);
     }
 }
