4f94e53d6570770e2f8f45209fd27823e84a0be1
mmoo <knr524@gmail.com>
2023-04-19 15:59:29 +0900

    Time: 1 ms (99.95%), Space: 42.7 MB (77.89%) - LeetHub

--- 0090-subsets-ii/0090-subsets-ii.java
+++ 0090-subsets-ii/0090-subsets-ii.java
@@ -1,23 +1,21 @@
 class Solution {
     public List<List<Integer>> subsetsWithDup(int[] nums) {
-        Set<List<Integer>> answer = new HashSet<>();
-        List<Integer> track = new ArrayList<>();
+        Arrays.sort(nums);
+        List<List<Integer>> answer = new ArrayList<>();
 
-        backTrack(nums,0,track, answer);
+        backTrack(nums,0,new ArrayList<>(), answer);
         
-        return new ArrayList<>(answer);
+        return answer;
     }
     
-    public void backTrack(int[] nums, int idx, List<Integer> track, Set<List<Integer>> answer) {
-        if(idx < nums.length) {
-            track.add(nums[idx]);
-            backTrack(nums,idx+1,track,answer);
+    public void backTrack(int[] nums, int idx, List<Integer> track, List<List<Integer>> answer) {
+        answer.add(new ArrayList<>(track));
+        for(int i=idx;i< nums.length;i++) {
+            /* 이전 idx의 숫자가 같다면 이미 추가된 경우이므로 건너뛴다 */
+            if(i!=idx && nums[i]==nums[i-1]) continue;
+            track.add(nums[i]);
+            backTrack(nums,i+1,track,answer);
             track.remove(track.size()-1);
-            backTrack(nums,idx+1,track,answer);
-        }else{
-            List<Integer> rs = new ArrayList<>(track);
-            Collections.sort(rs);
-            answer.add(rs);
         }
     }
 }
