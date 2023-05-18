5218363fe6a5b7efcf0e70c39e21f50f77462bef
mmoo <knr524@gmail.com>
2023-05-18 15:17:04 +0900

    Time: 122 ms (66.35%), Space: 43.3 MB (60.97%) - LeetHub

--- 0030-substring-with-concatenation-of-all-words/0030-substring-with-concatenation-of-all-words.java
+++ 0030-substring-with-concatenation-of-all-words/0030-substring-with-concatenation-of-all-words.java
@@ -1,24 +1,26 @@
 class Solution {
     public List<Integer> findSubstring(String s, String[] words) {
         List<Integer> answer = new ArrayList<>();
+        HashMap<String, Integer> wordCount = new HashMap<>();
+        for(String w:words) {
+            wordCount.put(w,wordCount.getOrDefault(w,0)+1);
+        }
 
-        int wLen = words[0].length();
-        int lineLen = wLen* words.length;
-        int end =lineLen;
-        int start = 0;
-        
-        while(end <= s.length()) {
-            String line = s.substring(start,end);
+        int wordLen = words[0].length();
+        int wordListLen = words.length;
 
-            List<String> wList = new ArrayList<>(Arrays.asList(words));
-            for(int k=0;k<line.length();k+=wLen) {
-                String w = line.substring(k,k+wLen);
-                if(wList.contains(w)) wList.remove(w);
-                else break;
+        for(int i=0;i<=s.length()-wordLen*wordListLen;i++) {
+            HashMap<String,Integer> seenWords = new HashMap<>();
+            for(int j=0;j<wordListLen;j++) {
+                int start = i+j*wordLen;
+                String sub = s.substring(start, start+wordLen);
+                if(wordCount.containsKey(sub)) {
+                    seenWords.put(sub, seenWords.getOrDefault(sub,0)+1);
+                    if(seenWords.get(sub) > wordCount.get(sub)) break;
+                }else break;
+
+                if(j+1 == wordListLen) answer.add(i);
             }
-            if(wList.isEmpty()) answer.add(start);
-            start++;
-            end++;
         }
         return answer;
     }
