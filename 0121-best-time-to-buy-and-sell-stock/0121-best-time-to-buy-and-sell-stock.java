b3076a0036ee67168d8f698c158e520902b8793d
mmoo <knr524@gmail.com>
2023-05-19 21:50:20 +0900

    Time: 2 ms (93.41%), Space: 61.4 MB (5.13%) - LeetHub

---
+++ 0121-best-time-to-buy-and-sell-stock/0121-best-time-to-buy-and-sell-stock.java
@@ -0,0 +1,12 @@
+class Solution {
+    public int maxProfit(int[] prices) {
+        int answer = 0;
+
+        int minPrice = Integer.MAX_VALUE;
+        for(int i=0;i<prices.length;i++) {
+            if(prices[i] < minPrice) minPrice = prices[i];
+            else if(prices[i] - minPrice > answer) answer = prices[i] - minPrice;
+        }
+        return answer;
+    }
+}
