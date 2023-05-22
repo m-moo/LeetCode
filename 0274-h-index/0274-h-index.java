b9da7022cedeeca65dd8e981fc4e2c67f58549e7
mmoo <knr524@gmail.com>
2023-05-22 22:48:08 +0900

    Time: 0 ms (100.00%), Space: 40.3 MB (88.52%) - LeetHub

--- 0274-h-index/0274-h-index.java
+++ 0274-h-index/0274-h-index.java
@@ -1,10 +1,15 @@
 class Solution {
     public int hIndex(int[] citations) {
-        Arrays.sort(citations);
-        int idx = 0;
-        int len = citations.length;
-        while(idx < len && citations[len-1-idx] > idx) idx++;
-        
-        return idx;
+        int count = 0;
+        int len = citations.length;;
+        int[] map = new int[len+1];
+
+        for(int c:citations) ++map[Math.min(c,len)];
+
+        for(int i=len;i>=0;i--) {
+            count += map[i];
+            if(count >= i) return i;
+        }
+        return 0;
     }
 }
