1b82396facbed7ddb661b929e9ae57d07fc69123
mmoo <knr524@gmail.com>
2023-06-18 16:17:22 +0900

    Time: 0 ms (100.00%), Space: 43.3 MB (42.44%) - LeetHub

--- 0116-populating-next-right-pointers-in-each-node/0116-populating-next-right-pointers-in-each-node.java
+++ 0116-populating-next-right-pointers-in-each-node/0116-populating-next-right-pointers-in-each-node.java
@@ -23,34 +23,19 @@ class Node {
 
 class Solution {
     public Node connect(Node root) {
-        List<List<Node>> levelNodes = new ArrayList<>();
-        bfs(root,levelNodes,0);
-
-        for(List<Node> nodes:levelNodes) {
-            for(int i=0;i<nodes.size();i++) {
-                Node curr = nodes.get(i);
-                Node next = null;
-                if(i+1!= nodes.size()) next = nodes.get(i+1);
-                curr.next = next;
+        if(root == null)
+            return null;
+        Node current = root;
+        while(current.left!=null){
+            Node temp = current;
+            while(current!=null){
+                current.left.next = current.right;
+                current.right.next = current.next == null ? null : current.next.left;
+                current = current.next;
             }
+            current = temp.left;
         }
 
         return root;
-    }
-
-    public void bfs(Node curr, List<List<Node>> levelNodes, int depth) {
-        if(curr == null) return;
-        if(levelNodes.size() == depth) {
-            List<Node> values = new ArrayList<>();
-            values.add(curr);
-            levelNodes.add(values);
-        }else {
-            List<Node> values = levelNodes.get(depth);
-            values.add(curr);
-            levelNodes.set(depth, values);
-        }
-
-        bfs(curr.left, levelNodes, depth+1);
-        bfs(curr.right, levelNodes, depth+1);
     }
 }
