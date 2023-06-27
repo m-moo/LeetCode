e9ee9c1770b977c3a713ca8de54312dcf3caaecd
mmoo <knr524@gmail.com>
2023-06-27 23:35:39 +0900

    Time: 23 ms (67.00%), Space: 45.4 MB (51.25%) - LeetHub

--- 0077-combinations/0077-combinations.java
+++ 0077-combinations/0077-combinations.java
@@ -1,19 +1,20 @@
 class Solution {
+    List<List<Integer>> answer = new ArrayList<>();
+
     public List<List<Integer>> combine(int n, int k) {
-        List<List<Integer>> answer = new ArrayList<>();
-
-        backTrack(answer, n,k,1,new ArrayList<>());
-
+        if(n <= 0 || k <= 0 || k > n) return answer;
+        backTrack(n,k,1,new ArrayList<>());
         return answer;
     }
-    public void backTrack(List<List<Integer>> answer, int n, int k, int start, List<Integer> comb) {
+    
+    public void backTrack(int n, int k, int start, List<Integer> comb) {
         if(k==0) {
             answer.add(new ArrayList<Integer>(comb));
             return;
         }
         for(int i=start;i<=n;i++) {
             comb.add(i);
-            backTrack(answer,n,k-1,i+1,comb);
+            backTrack(n,k-1,i+1,comb);
             comb.remove(comb.size()-1);
         }
     }
