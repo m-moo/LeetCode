3ec2325ed10dde72dbfd3812bc02fc6d98c539f0
mmoo <knr524@gmail.com>
2023-08-11 17:55:46 +0900

    Time: 0 ms (100%), Space: 41.3 MB (43.86%) - LeetHub

--- 0017-letter-combinations-of-a-phone-number/0017-letter-combinations-of-a-phone-number.java
+++ 0017-letter-combinations-of-a-phone-number/0017-letter-combinations-of-a-phone-number.java
@@ -1,28 +1,31 @@
 class Solution {
-    char[][] alph = {{},{},
-        {'a','b','c'},{'d','e','f'},{'g','h','i'},
-        {'j','k','l'},{'m','n','o'},{'p','q','r','s'},
-        {'t','u','v'},{'w','x','y','z'}
-    };
+    String[][] pad;
+
     public List<String> letterCombinations(String digits) {
         List<String> answer = new ArrayList<>();
-
         if(digits.length() == 0) return answer;
+        pad = new String[][]{
+            {},{},{"a","b","c"},{"d","e","f"},
+            {"g","h","i"},{"j","k","l"},{"m","n","o"},
+            {"p","q","r","s"},{"t","u","v"},{"w","x","y","z"}
+        };
 
-        char[] arr = digits.toCharArray();
-        backTrack(arr, 0, new StringBuilder(), answer);
-        
+        backTrack("",0, digits, answer);
+
         return answer;
     }
-    
-    public void backTrack(char[] digits, int idx, StringBuilder pushed, List<String> answer) {
-        if(idx >= digits.length) {answer.add(pushed.toString()); return;}
 
-        int button = digits[idx]-'0';
-        for(int i=0;i<alph[button].length;i++) {
-            pushed.append(alph[button][i]);
-            backTrack(digits, idx+1, pushed, answer);
-            pushed.delete(pushed.length()-1,pushed.length());
+    public void backTrack(String comb, int depth, String digits, List<String> answer) {
+        if(depth == digits.length()) {
+            answer.add(comb);
+            return;
+        }
+
+        int digit = digits.charAt(depth) - '0';
+        for(int i=0;i<pad[digit].length;i++) {
+            StringBuilder sb = new StringBuilder(comb);
+            sb.append(pad[digit][i]);
+            backTrack(sb.toString(), depth+1, digits, answer);
         }
     }
 }
