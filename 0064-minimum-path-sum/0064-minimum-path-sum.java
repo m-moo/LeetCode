4429f90c6dc4713f711d4351addf55a590bca527
mmoo <knr524@gmail.com>
2023-10-25 19:59:49 +0900

    Time: 3 ms (63.64%), Space: 43.8 MB (43.96%) - LeetHub

---
+++ 0064-minimum-path-sum/0064-minimum-path-sum.java
@@ -0,0 +1,19 @@
+class Solution {
+    public int minPathSum(int[][] grid) {
+        int row = grid.length;
+        int col = grid[0].length;
+        for(int i=1;i<row;i++) {
+            grid[i][0] += grid[i-1][0];
+        }
+        for(int i=1;i<col;i++) {
+            grid[0][i] += grid[0][i-1];
+        }
+        for(int i=1;i<row;i++) {
+            for(int j=1;j<col;j++) {
+                int curr = grid[i][j];
+                grid[i][j] = Math.min(curr+grid[i-1][j],curr+grid[i][j-1]);
+            }
+        }
+        return grid[row-1][col-1];
+    }
+}
