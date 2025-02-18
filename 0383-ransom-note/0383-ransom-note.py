343ffed98f3465f48d9d99429ce2719425accc34
mmoo <knr524@gmail.com>
2025-02-18 23:59:11 +0900

    Time: 2 ms (98.31%), Space: 17.8 MB (82.21%) - LeetHub

--- 0383-ransom-note/0383-ransom-note.py
+++ 0383-ransom-note/0383-ransom-note.py
@@ -1,13 +1,7 @@
 class Solution:
     def canConstruct(self, ransomNote: str, magazine: str) -> bool:
-        mem = [0] * 26
-        for i in range(len(magazine)):
-            mem[ord(magazine[i]) - ord('a')] += 1
-        
-        for i in range(len(ransomNote)):
-            idx = ord(ransomNote[i]) - ord('a')
-            mem[idx] -= 1
-            if mem[idx] < 0:
+        for c in ransomNote:
+            if c not in magazine:
                 return False
-
+            magazine = magazine.replace(c,"",1)
         return True
