44f7fba2bec8db57aed880ea48e474c2641f162b
mmoo <knr524@gmail.com>
2025-02-19 22:10:35 +0900

    Time: 7 ms (57.11%), Space: 19.4 MB (52%) - LeetHub

--- 0169-majority-element/0169-majority-element.py
+++ 0169-majority-element/0169-majority-element.py
@@ -3,7 +3,7 @@ from collections import Counter
 class Solution:
     def majorityElement(self, nums: List[int]) -> int:
         count_nums = Counter(nums)
-        for num, cnt in count_nums.most_common():
+        for num, cnt in count_nums.items():
             if cnt > len(nums) // 2: 
                 return num
         
