1d8f1c21a34d4ff9248420dc0ef2f284929240fd
mmoo <knr524@gmail.com>
2025-02-20 15:28:49 +0900

    Time: 178 ms (41.67%), Space: 20.6 MB (50.36%) - LeetHub

--- 0542-01-matrix/0542-01-matrix.py
+++ 0542-01-matrix/0542-01-matrix.py
@@ -1,32 +1,25 @@
 class Solution:
     def updateMatrix(self, mat: List[List[int]]) -> List[List[int]]:
         m, n = len(mat), len(mat[0])
-        visited = set()
         dirs = [(-1, 0), (0, 1), (1, 0), (0 ,-1)]
-        
-        def bfs(i: int, j: int):
-            queue = []
-            queue.append((i, j))
+        res = [[m * n] * n for _ in range(m)]
 
-            while queue:
-                x, y = queue.pop()
-                visited.add((x, y))
-                
-                min_distance = m * n
-                for dir_x, dir_y in dirs:
-                    new_x, new_y = x + dir_x, y + dir_y
-                    if 0 <= new_x < m and 0 <= new_y < n:
-                        if mat[new_x][new_y] == 0:
-                            min_distance = 0
-                            break
-                        if (new_x, new_y) not in visited:
-                            bfs(new_x, new_y)
-                        min_distance = min(min_distance, mat[new_x][new_y])
-                mat[x][y] = min_distance + 1
-        
+        queue = deque()
+        # init 0 cells
         for i in range(m):
             for j in range(n):
-                if mat[i][j] != 0 and (i, j) not in visited:
-                    bfs(i, j)
+                if mat[i][j] == 0:
+                    res[i][j] = 0
+                    queue.append((i, j))
 
-        return mat
+        while queue:
+            x, y = queue.popleft()
+            for dir_x, dir_y in dirs:
+                new_x, new_y = x + dir_x, y + dir_y
+
+                if 0 <= new_x < m and 0 <= new_y < n:
+                    if res[new_x][new_y] > res[x][y]:
+                        res[new_x][new_y] = res[x][y] + 1
+                        queue.append((new_x, new_y))
+
+        return res
